33c1edb6e4da32b1deed7f3f231867a2f8b911a2
Daria <61759834+jezowska@users.noreply.github.com>
2021-05-02 16:39:14 +0200

    repetition code added

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-
-
-import numpy as np
 import BitString as bs
+import RepetitionBitCoder as rbc
 import ParityBitCoder as pbc
 
-def main():
-    size = 12
-    bitstring = bs.BitString(size)
-    print(bitstring.returnArray())
-main()
+size = 12
+bitstring = bs.BitString(size)
+print(bitstring.returnArray())
+
+repetition = rbc.RepetitionBitCoder(bitstring.bool_arr);
+repetition.coding();
+
+parity = pbc.ParityBitCoder(size, bitstring.bool_arr)
+parity.Coding();
+
+print("Bez kodowania:")
+print(bitstring.bool_arr)
+
+print("Potrajanie bitów:")
+print(repetition.return_array())
+
+print("Parzystoćś bitów:")
+print(parity.returnArray())
