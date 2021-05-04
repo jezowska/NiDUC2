88e8cc603ee4d95fa01ad00c738d7694035fd6f9
Daria <61759834+jezowska@users.noreply.github.com>
2021-05-04 10:29:41 +0200

    noise added

--- noise.py
+++ noise.py
@@ -4,14 +4,13 @@ class Noise(object):
     def __init__(self, bool_arr):
         self.size = len(bool_arr)
         self._bool_arr = bool_arr
-        self.sample_arr = [0, 1]
         self.new_array = []
 
     def adding_noise_inc(self):
         prob = 0.5
         print(self._bool_arr)
         for i in range(self.size):
-            x = np.random.choice(self.sample_arr)
+            x = np.random.uniform(0.0, 1.0)
             if( x > prob ):
                 if(self._bool_arr[i] == 0):
                     self.new_array.append(1);
@@ -29,7 +28,7 @@ class Noise(object):
     def adding_noise_dec(self):
         prob = 0.5
         for i in range(self.size):
-            x = np.random.choice(self.sample_arr)
+            x = np.random.uniform(0.0, 1.0)
             if (x > prob):
                 if (self._bool_arr[i] == 0):
                     self.new_array.append(1);
@@ -45,7 +44,7 @@ class Noise(object):
     def adding_noise(self):
         prob = 0.5
         for i in range(self.size):
-            x = np.random.choice(self.sample_arr)
+            x = np.random.uniform(0.0, 1.0)
             if (x > prob):
                 if (self._bool_arr[i] == 0):
                     self.new_array.append(1);
