1ef19f85e72e22fbc87c46644f829840de1002d8
Zo5o <pluciennik.jakub98@gmail.com>
2021-05-30 12:11:15 +0200

    update

--- RBDtester.py
+++ RBDtester.py
@@ -6,38 +6,42 @@ import ParityBitCoder as pbc
 import RepetitionBitDecoder as rbd
 import noise
 
-niewykryte = 0
-nienaprawialne = 0
-bledy = 0
+
 size = 100000
-
-# bazowy ciąg bitów
-bitstring = bs.BitString(size)
-#print("Bez kodowania:")
-#print(bitstring.returnArray())
-
-#kod powtarzalny
-repetition = rbc.RepetitionBitCoder(bitstring.bool_arr);
-repetition.coding();
-coded_array = repetition.coded_arr
-#print("Potrajanie bitów:")
-#print(repetition.return_array())
-
-#kod powtarzalny - zaszumiony
-repetition_noised = noise.Noise(coded_array)
-repetition_noised.adding_noise()
-#print("Potrajanie bitów z szumem:")
-#print(repetition_noised.new_array)
-
-rep_n = rbd.RepetitionBitDecoder(repetition_noised.new_array)
-rep_n.decoding();
-#print("Potrajanie bitów - dekoder:")
-#print(rep_n.return_array())
-indeks = 0
-pom = 0
-
+time = 10
 wynik = []
-for i in range(0, 3, 1):
+for i in range(time):
+    print(i)
+    niewykryte = 0
+    nienaprawialne = 0
+    bledy = 0
+
+    # bazowy ciąg bitów
+    bitstring = bs.BitString(size)
+    #print("Bez kodowania:")
+    #print(bitstring.returnArray())
+
+    #kod powtarzalny
+    repetition = rbc.RepetitionBitCoder(bitstring.bool_arr)
+    repetition.coding()
+    coded_array = repetition.coded_arr
+    #print("Potrajanie bitów:")
+    #print(repetition.return_array())
+
+    #kod powtarzalny - zaszumiony
+    repetition_noised = noise.Noise(coded_array)
+    repetition_noised.adding_noise()
+    #print("Potrajanie bitów z szumem:")
+    #print(repetition_noised.new_array)
+
+    rep_n = rbd.RepetitionBitDecoder(repetition_noised.new_array)
+    rep_n.decoding()
+    #print("Potrajanie bitów - dekoder:")
+    #print(rep_n.return_array())
+    indeks = 0
+    pom = 0
+
+
     for i in range(0, (size * 3), 1):
         pom += 1
         if bitstring.bool_arr[int(i/3)] != repetition_noised.new_array[i]:
@@ -54,7 +58,7 @@ for i in range(0, 3, 1):
             nienaprawialne += 1
     wynik.append([rep_n.wykryte, nienaprawialne, niewykryte, bledy])
 
-dane = (pd.DataFrame(wynik))
+dane = (pd.DataFrame(wynik, columns= ["wykryte", "nienaprawialne", "niewykryte", "bledy"]))
 dane.to_csv('Dane.csv', index=True, sep=';')
 
 print("---")
